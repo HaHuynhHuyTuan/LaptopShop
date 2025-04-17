d0f1ba9778d3946c9686abd4f109700668f2fb8a
HaHuynhHuyTuan <hahuynhhuytuan123@gmail.com>
2025-04-17 21:47:15 +0700

    validate register

--- src/main/java/com/example/demo/controller/client/HomePageController.java
+++ src/main/java/com/example/demo/controller/client/HomePageController.java
@@ -4,9 +4,12 @@ import com.example.demo.domain.Product;
 import com.example.demo.domain.dto.RegisterDTO;
 import com.example.demo.service.ProductService;
 import com.example.demo.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -45,7 +48,16 @@ public class HomePageController {
     }
 
     @PostMapping("/register")
-    public String handleRegister(@ModelAttribute("registerUser") RegisterDTO registerDTO) {
+    public String handleRegister(
+            @ModelAttribute("registerUser") @Valid RegisterDTO registerDTO,
+            BindingResult bindingResult
+    ) {
+        List<FieldError> errors = bindingResult.getFieldErrors();
+        for (FieldError error : errors) {
+            System.out.println(">>>>" + error.getField() + "-" +
+                    error.getDefaultMessage());
+        }
+
         User user = this.userService.registerDTOtoUser(registerDTO);
 
         String hashPassword = passwordEncoder.encode(user.getPassword());
